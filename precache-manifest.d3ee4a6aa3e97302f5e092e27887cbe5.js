d7c38c185edfb6fd6b1e90a6657468f6f42d9f48
Nemanja <nemanja94email@gmail.com>
2020-09-21 18:18:45 +0200

    Updates

---
+++ precache-manifest.d3ee4a6aa3e97302f5e092e27887cbe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eda790988dc82644c535fea7c70e4c1",
+    "url": "/react-monsters-exercise/index.html"
+  },
+  {
+    "revision": "001112881530dd77e762",
+    "url": "/react-monsters-exercise/static/css/main.82be34be.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monsters-exercise/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-exercise/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001112881530dd77e762",
+    "url": "/react-monsters-exercise/static/js/main.2757d88d.chunk.js"
+  },
+  {
+    "revision": "10d1b4c2f3b6e9baa75c",
+    "url": "/react-monsters-exercise/static/js/runtime-main.9e00c18c.js"
+  }
+]);
